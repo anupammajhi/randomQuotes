ad95371b660526cc8e79bd00739b2f7aef3eb90c
Anupam Majhi <majhi.anupam@gmail.com>
2017-01-15 02:38:42 +0530

    FIRST WORKING CODE

---
+++ main.js
@@ -0,0 +1,31 @@
+function getQuote(){
+  $.ajax({
+    method:"GET",
+    data:{method:"getQuote",lang:"en",format:"jsonp",jsonp:"theQuote"},
+    dataType:"jsonp",
+    jsonpCallback:"theQuote",
+    url:"http://api.forismatic.com/api/1.0/",
+    success : function(data){
+      $(".quote").html(data.quoteText);
+      if(data.quoteAuthor !== ""){
+        $(".quoteby").html("- "+data.quoteAuthor);
+      }
+      else{
+        $(".quoteby").html("- Anonymous");
+      }
+
+      $("#btn-tweet").click(function(){
+        var tweeturl = "https://twitter.com/intent/tweet?text=" + data.quoteText + "%0D- " + data.quoteAuthor + "%0Dvia @anupamfx";
+        window.open(tweeturl,"Tweet","width:800,height:800,toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0,resizable=0");
+      });
+
+    }
+  });
+}
+
+$(document).ready(function(){
+  $(".main").innerHeight($(window).innerHeight());
+  getQuote();
+});
+
+$("#btn-next").click(getQuote);
